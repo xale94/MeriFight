4061f17b0287e8c14c3ad4f6f2933e61390966a6
xale1994@gmail.com <xale1994@gmail.com>
2017-05-23 17:13:55 +0200

    More heal bar tests

--- src/app/models/HealBar.js
+++ src/app/models/HealBar.js
@@ -1,13 +1,13 @@
-function HealBar(_healBar) {
+function HealBar(_healBarSettings) {
     try {
-        this.setPosX(_healBar.posX);
-        this.setPosY(_healBar.posY);
-        this.setWidth(_healBar.width);//bigger than 0
-        this.setHeight(_healBar.height);//bigger than 0
-        this.setContext(_healBar.context);
-        this.setBackground(_healBar.background);
+        this.setPosX(_healBarSettings.posX);
+        this.setPosY(_healBarSettings.posY);
+        this.setWidth(_healBarSettings.width);//bigger than 0
+        this.setHeight(_healBarSettings.height);//bigger than 0
+        this.setContext(_healBarSettings.context);
+        this.setBackground(_healBarSettings.background);
         if (_scenary.limits)
-            this.setLimits(_healBar.limits); 
+            this.setLimits(_healBarSettings.limits); 
     } catch (e) {
         throw (e);
     }
